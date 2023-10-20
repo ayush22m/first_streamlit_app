2c7fcfbbf6d0398cc937e06b8dbae8dcff3e6190
ayush22m <107382118+ayush22m@users.noreply.github.com>
2023-10-20 13:12:26 +0530

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Uttapam')
+streamlit.text('idli')
+streamlit.text('dosa')
