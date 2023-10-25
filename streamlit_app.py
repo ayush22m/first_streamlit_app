315fd4180f8036150a7f9247f6c07cdc3b675029
ayush22m <107382118+ayush22m@users.noreply.github.com>
2023-10-25 15:17:40 +0530

    Update streamlit_app.py
    Add fruitname in list

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("the fruit load list contains:")
 streamlit.text(my_data_row)
+
+fruit_choice1 = streamlit.text_input('What fruit would you like information about?','kiwi')
+streamlit.add('thanks for adding', fruit_choice1)
