99b9a5f2169c405f939667f7b44b2cbcf5ce107a
ayush22m <107382118+ayush22m@users.noreply.github.com>
2023-10-25 16:39:54 +0530

    Update streamlit_app.py
    replace code with if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import requests
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -19,20 +21,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+  if not fruit_choice:
+    streamlit.error("please select fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error();
 streamlit.write('The user entered ', fruit_choice)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -43,4 +44,4 @@ streamlit.text(my_data_row)
 
 add_my_fruit = streamlit.text_input('What would you like to add?','kiwi')
 streamlit.write('thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values('"+ add_my_fruit +'")")
+my_cur.execute("insert into fruit_load_list values('')")
